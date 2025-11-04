0ddbcfde4b91f194123971633fe89a8dae0f48a5
Bonbia <bonbia75@gmail.com>
2025-11-04 16:45:20 +0100

    Ajt fonctions de base

--- BM3D.py
+++ BM3D.py
@@ -11,3 +11,38 @@ from skimage import io as skio
 import IPython
 from skimage.transform import rescale
 from skimage.io import imread
+
+
+
+### 
+
+
+im=imread('img/pyramide.tif')
+
+
+#Fonction de bruit gaussien cf tp 
+def noisegauss(im,br):
+    """ Cette fonction ajoute un bruit blanc gaussier d'ecart type br
+       a l'image im et renvoie le resultat"""
+    imt=np.float32(im.copy())
+    sh=imt.shape
+    bruit=br*np.random.randn(*sh)
+    imt=imt+bruit
+    return imt
+
+#Fonction affichage
+def viewimage(im, normalize=True,z=1,order=0,titre='',displayfilename=False):
+    imin=im.copy().astype(np.float32)
+    imin = rescale(imin, z, order=order)
+    if normalize:
+        imin-=imin.min()
+        if imin.max()>0:
+            imin/=imin.max()
+    else:
+        imin=imin.clip(0,255)/255 
+    imin=(imin*255).astype(np.uint8)
+    filename=tempfile.mktemp(titre+'.png')
+    if displayfilename:
+        print (filename)
+    plt.imsave(filename, imin, cmap='gray')
+    IPython.display.display(IPython.display.Image(filename))
